34f2ebe54257dba7a4ea5252af976e795b1ed890
Amit Das <40338644+A70das@users.noreply.github.com>
2020-04-30 19:53:10 +0530

    Create Only EncoderTest.ino

---
+++ Only EncoderTest/Only EncoderTest.ino
@@ -0,0 +1,16 @@
+#include <Encoder.h>
+Encoder myEnc(39, 3);
+
+long oldPosition  = -999;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
